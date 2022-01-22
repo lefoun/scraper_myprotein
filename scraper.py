f162f477295a9a9af2d7e043ccc040d5b91e0560
Noufel <noufel.ammari@hotmail.com>
2022-01-22 17:18:40 +0100

    script to check if there's a promotion in my protein

--- scraper.py
+++ scraper.py
@@ -1,3 +1,16 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open('home.html', 'r')
+def find_pormotion():
+    html_txt = requests.get('https://fr.myprotein.com/').text
+
+    soup = BeautifulSoup(html_txt, 'lxml')
+    banner_containing_promotions = soup.find(class_="stripBanner").text
+    promotions_list = ['35', '40', '45', '50', '55', '60']
+
+    for promotion in promotions_list:
+        if promotion in banner_containing_promotions:
+            print(f"Il y a une réduction de {promotion} chez Myprotein")
+            return
+
+
